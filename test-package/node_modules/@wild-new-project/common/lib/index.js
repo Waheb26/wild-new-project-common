6375287f1e71f36dc6bb31dfd024a0f39ad77273
Waheb26 <cherwaheb@gmail.com>
2024-05-14 15:15:35 +0200

    add export and :github workflow)

--- test-package/node_modules/@wild-new-project/common/lib/index.js
+++ test-package/node_modules/@wild-new-project/common/lib/index.js
@@ -1,2 +1,5 @@
 "use strict";
-const helloWilder = (wilder) => `Hello ${wilder.name} from ${wilder.city}`;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWilder = void 0;
+const helloWilder = (wilder) => `Hello ${wilder.name} from ${wilder.city} :)`;
+exports.helloWilder = helloWilder;
